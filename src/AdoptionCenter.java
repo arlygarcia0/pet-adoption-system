13f244f4b17fd2166ef07ece2792a23664cb0774
jennikarowo <jennifer.aguilar05@utrgv.edu>
2026-04-29 05:15:27 +0000

    Updated the newPet class. Will need to separate the adoptionCenter file into different classes/files to respect SOLID

--- src/AdoptionCenter.java
+++ src/AdoptionCenter.java
@@ -1,3 +1,5 @@
+/*Separte the classes on different files */
+
 import Pets.*;
 
 public class AdoptionCenter {
@@ -7,7 +9,11 @@ public class AdoptionCenter {
     public AdoptionCenter() {
         //Create a table for pets in shelter
         db.updateDatabase("CREATE TABLE IF NOT EXISTS Pets (id INT PRIMARY KEY, name VARCHAR(50), type VARCHAR(50), breed VARCHAR(50), age INT, adopted BOOLEAN)");
-        
+    }
+}
+
+public class addValues {
+    public addValues () {
         //Insert pets in the table
         db.updateDatabase("INSERT INTO Pets (id, name, type, breed, age, adopted) VALUES (01, 'Soda', 'Dog', 'Doberman', 3, 0)");
         db.updateDatabase("INSERT INTO Pets (id, name, type, breed, age, adopted) VALUES (02, 'Buddy', 'Dog', 'Labrador', 2, 1)");
@@ -23,16 +29,26 @@ public class AdoptionCenter {
         db.updateDatabase("INSERT INTO Pets (id, name, type, breed, age, adopted) VALUES (12, 'Milo', 'Cat', 'Ragdoll', 1, 1)");
         db.updateDatabase("INSERT INTO Pets (id, name, type, breed, age, adopted) VALUES (13, 'Chloe', 'Cat', 'British Shorthair', 6, 0)");
     }
+}
 
-    //Add a pet to the database
-    public void addPet(Pet pet) {
-        db.updateDatabase("INSERT INTO Pets (id, name, type, breed, age, adopted) VALUES (pet.id, pet.name, pet.type, pet.breed, pet.age, pet.adopted)");
+// Adding a new pet to the shelter using *Polumorphism* -> Overloading
+public class newPet {
+    // Add Dog
+    public void addPet(Dog dog) {
+        db.updateDatabase("INSERT INTO Pets (name, type, breed, age, adopted) VALUES ("+ dog.getName() + ", 'Dog',"+ dog.getBreed() + ", "+ dog.getAge() + ", 0)");
     }
-
+    // Add Cat
+    public void addPet(Cat cat) {
+        db.updateDatabase("INSERT INTO Pets (name, type, breed, age, adopted) VALUES ("+ cat.getName() + ", 'Cat',"+ cat.getBreed() + ", "+ cat.getAge() + ", 0)");
+    }
+}
+    
+public class info {
     // Display all availble pets
     public void displayPets() {
         db.readDatabase("SELECT * FROM Pets WHERE adopted = 0");
     }
+}
 
     // Adopt A pet 
     public void adoptPet(int petID) {
--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,46 @@ import Pets.*;
 
 public class Main {
     public static void main(String[] args) {
+        // 1. Initialize system: 
+        //    Create pets table and add current pets in the shelter
+        
+        // 2. Create new user:
+        //    Ask for adopter name -> Check if its already in the system
+        //    if not then add it to the table with currently 0 pets
+        //    if yes, then select its tables
+
+        // 3. Display available pets:
+        //    Show pets where adopted = false
+
+        // 4. Adopt a pet
+        //    User selects pet -> Choose by breed or they can choose specifically by name/id
+        //    Update pet's database and add it to user's table
+        
+        // 5. Show adopted pets by user
+
+        // 6. Add new pet to the shelter
+        //    Add a dog then add a cat, check that both work
+
+        // 7. Update pet information
+        //    Pet got older -> update age
+
+        // 8. Show shelter status
+
+        // 9. End program
+
         Scanner input = new Scanner(System.in);
 
         AdoptionCenter shelter = new AdoptionCenter();
 
+
+        //Create a user
         System.out.println("Enter your name: ");
         String name = input.nextLine();
 
         User user = new User(name);
 
+
+        //Show them pets available and adopt a pet
         int choice;
 
         do {
@@ -33,6 +64,13 @@ public class Main {
 
         user.viewAdoptedPets();
 
+        //New user,  check ho wmany pets they have adopted
+
+        //New pet in shelter, add it to table
+
+        //Pet turn one year older, update its age
+
+        //End code
         input.close();
     }
 }
